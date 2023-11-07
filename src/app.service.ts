ba2806c412e14943a4ce84eb97d85bd0e0ad4aa0
Léonard <shinwa.lj@gmail.com>
2023-11-07 15:00:12 +0100

    Partie 3

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { HelloMessage } from './HelloMessage';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(name:string): HelloMessage {
+    return {hello: name};
+    
   }
 }
